22a718b89912c1ba3a16e32a02d3417fe0dd286a
Akshat Srivastava <akshatsrivastava14@gmail.com>
2018-01-14 16:40:19 +0530

    Create BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *x,int *y)
+{
+   int temp=*x;
+   *x=*y;
+   *y=temp;
+}
+
+void bubblesort(int arr[],int n)
+{
+  int i,j,opcount=0;
+  for(i=0;i<n-1;i++)
+  {
+  	for(j=0;j<n-1-i;j++)
+  	{
+  		opcount++;
+  		if(arr[j]>arr[j+1])
+  		{
+  			swap(&arr[j],&arr[j+1]);
+            
+  		}
+  	}
+  }
+  	printf("Sorted Array after bubblesort:\n");
+	for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+  printf("Opcount of bubble sort:%d\n",opcount);
+}
+
+int main()
+{
+	int a[100],n,i;
+	printf("Enter number of elements:\n");
+	scanf("%d",&n);
+    printf("Enter elements:\n");
+    for(i=0;i<n;i++)
+    {
+    	scanf("%d",&a[i]);
+    }
+	bubblesort(a,n);
+	return 0;
+}
