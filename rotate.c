97d8b53271be2ec0d94f3191972ac4ecb38de0d3
Akshat Srivastava <akshatsrivastava14@gmail.com>
2018-01-14 16:21:26 +0530

    Add files via upload

---
+++ rotate.c
@@ -0,0 +1,49 @@
+
+#include<stdio.h>
+
+void leftrotate(int arr[],int d,int n)
+{ 
+   int b[50];
+   int temp,i;
+   for(i=n-1;i>=0;i--)
+   {
+   	 temp=arr[i];
+   	 if(i>=d)
+   	 {
+   	 	b[i-d]=temp;
+   	 }
+   	 else
+   	 {
+   	 	b[n-d+i]=temp;
+   	 }
+   }
+   for(i=0;i<n;i++)
+   {
+   	arr[i]=b[i];
+   }
+}
+
+int main()
+{
+	int n,d;
+    int a[50];
+	printf("Enter the size of array:");
+	scanf("%d",&n);
+    int i;
+	printf("Enter the rotation number:");
+	scanf("%d",&d);
+	printf("Enter elements:\n");
+	for(i=0;i<n;i++)
+	{
+          scanf("%d",&a[i]);
+	}
+    leftrotate(a,d,n);    
+    printf("Rotated Array:\n");
+    for(i=0;i<n;i++)
+	{
+          printf("%d ",a[i]);
+	}
+	printf("\n");
+    return 0;
+
+}
